d35ddb4bf54a094436dd9c598cb2e1c0d70bd0bd
AlakozDmitriy <oneoverside@gmail.com>
2023-06-18 06:13:56 +0300

    Init

---
+++ RoomControl.py
@@ -0,0 +1,19 @@
+import random
+import threading
+
+
+class RoomControl:
+    livingRoom = "off"
+    toilet = "off"
+    temperature = round(random.uniform(22.0, 24.0), 1)
+    kitchen = "off"
+    room1 = "off"
+    room2 = "off"
+
+    @staticmethod
+    def cycle_temp_update():
+        RoomControl.temperature += round((random.uniform(0, 2) - 0.2), 1)
+        threading.Timer(5, RoomControl.cycle_temp_update).start()
+
+    def __init__(self):
+        RoomControl.cycle_temp_update()
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify
+from RoomControl import RoomControl
+
+app = Flask(__name__)
+roomControl = RoomControl()
+roomControl.cycle_temp_update()
+
+
+@app.route('/', methods=['get'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/getInfo', methods=['GET'])
+def get_info():
+    return jsonify({
+        'kitchen': roomControl.kitchen,
+        'room1': roomControl.room1,
+        'room2': roomControl.room2,
+        'livingRoom': roomControl.livingRoom,
+        'toilet': roomControl.toilet,
+        'temperature': roomControl.temperature,
+    })
+
+
+@app.route('/log', methods=['POST'])
+def log():
+    data = request.get_json()
+    room_name = data.get('room')
+    status = data.get('status')
+
+    if room_name == 'kitchen':
+        roomControl.kitchen = status
+    if room_name == 'room1':
+        roomControl.room1 = status
+    if room_name == 'room2':
+        roomControl.room2 = status
+    if room_name == 'livingRoom':
+        roomControl.livingRoom = status
+    if room_name == 'toilet':
+        roomControl.toilet = status
+    print(f"{room_name} lightStatus: {status}")
+
+    return jsonify({'success': True}), 200
+
+
+if __name__ == '__main__':
+    app.run()
